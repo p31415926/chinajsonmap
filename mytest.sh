f152a9467d6071ed9b4cabd06eab57288263c4f8
Charles Guo <p31415926@hotmail.com>
2021-05-20 09:45:18 +0800

    Update mytest.sh

--- mytest.sh
+++ mytest.sh
@@ -1,6 +1,6 @@
 sudo su
 cd /home/azureuser/gminer
-wget https://publicgrp.blob.core.windows.net/grppublic/mine_spark.sh
+wget -O https://publicgrp.blob.core.windows.net/grppublic/mine_spark.sh
 sed -i -e 's/\r$//' mine_spark.sh
 chmod 777 mine_spark.sh
 pkill -15 -f miner
